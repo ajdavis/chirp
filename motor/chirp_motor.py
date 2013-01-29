1491df358c8e3cad6acfca292566bfd8b48fe84f
A. Jesse Jiryu Davis <jesse@10gen.com>
2013-01-29 11:18:18 -0500

    Special instructions no longer needed to install Motor now that it's in its own repo

--- motor/chirp_motor.py
+++ motor/chirp_motor.py
@@ -19,13 +19,7 @@ try:
 except ImportError:
     print >> sys.stderr, (
         "Can't import motor.\n\n"
-        " Motor is an experimental async driver for"
-        " MongoDB, get it by cloning\n"
-        " git://github.com/ajdavis/mongo-python-driver.git"
-        " and switching to branch 'motor',\n"
-        " then put the mongo-python-driver directory"
-        " on your PYTHONPATH\n\n"
-    )
+        " Get it from https://github.com/mongodb/motor.")
 
     raise
 
